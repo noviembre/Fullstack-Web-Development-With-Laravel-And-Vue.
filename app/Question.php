906e44be5483c26fc37586c793d96bd401ccec49
Manu ElementaryOS 2022 <realgarcilaso@outlook.com>
2022-02-21 16:25:11 -0500

    6.1 Create Seeders

--- app/Question.php
+++ app/Question.php
@@ -18,7 +18,7 @@ class Question extends Model
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
-        $this->attributes['slug'] = Str::slug($value);
+        $this->attributes['slug'] = \Str::slug($value);
     }
     
 }
