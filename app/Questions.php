14e8501db445e1d159aa479d6ddd8f92ffceff65
Manu Elementary 2022 <realgarcilaso@outlook.com>
2022-03-04 09:34:33 -0500

    8.1 Question slug - Url new way

--- app/Questions.php
+++ app/Questions.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Questions extends Model
 {
-    //
+    protected $fillable = ['title', 'body'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    #================= Title Mutator
+    #---for title slug
+    public function setTitleAttribute($value): void
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = \Str::slug($value);
+    }
+
+
+
+
+    public function getUrlAttribute(): string
+    {
+        return route('questions.show',$this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
 }
